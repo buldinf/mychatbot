d5d2254ea1a700aa3303f6c7cef74b5c9c4dca54
Buldin Tobar, F <f.buldin.tobar@accenture.com>
2017-08-27 17:47:53 +0200

    node_modules and bootstrap removed

--- myapp/app.js
+++ myapp/app.js
@@ -15,10 +15,6 @@ app.get('/cover', function (req, res) {
 	res.sendFile(path +  'cover/index.html');
 });
 
-app.get('/cover2', function (req, res) {
-	res.sendFile(__dirname + '/bootstrap-4.0.0-beta/docs/4.0/examples/dashboard/cover/index.html');
-});
-
 app.get('/jade', function (req, res) {
   res.render('index', { title: 'Hey', message: 'Hello there!'});
 });
